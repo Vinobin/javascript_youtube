6fbaf7ac525860622a6e18104fa46561add19956
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2023-09-11 19:51:18 +0700

    V 1.2
    JAVASCRIPT : TIPE DATA

---
+++ js/tipedata.js
@@ -0,0 +1,22 @@
+let angka=10;
+// TIPE DATA NUMBER ADALAH BERISI ANGKA ATAU INTEGER
+let kata="saya belajar js";
+// TIPE DATA STRING ADALAH ADA TANDA PETIK DUA
+let pecahan=3.4;
+// TIPE DATA FLOAT ADALAH ADA TANDA KOMA
+console.log(angka);
+console.log(kata);
+console.log(typeof pecahan);
+console.log(1/0);
+console.log("JAVASCRIPT" / 2+5);
+let isi="lagi belajar";
+console.log(isi + kata);
+console.log(` "bintang" ${angka} ${kata} ${isi}`);
+console.log("bintang"+angka+kata+isi);
+let kondisi=true;
+// TIPE DATA BOOLEAN ADALAH TRUE OR FALSE
+console.log(kondisi);
+let age=null;
+console.log(age);
+let kosong;
+console.log(kosong);
