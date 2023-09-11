689000894350ec35e431693db5d31a8dff019059
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2023-09-11 19:30:35 +0700

    V 1.1
    JAVASCRIPT : VARIABEL DAN KONSTANTA

---
+++ js/variabel.js
@@ -0,0 +1,14 @@
+// KIRI ADALAH WADAH KANAN ADALAH ISI
+let angka;
+let kata = "saya belajar";
+const data = 100;
+angka=70;
+// KONSTANTA TIDAK BISA MERUBAH VARIABEL ISINYA
+console.log(angka);
+console.log(kata);
+console.log(data);
+angka=33;
+console.log(angka);
+let isi=5;
+console.log(typeof isi);
+// TYPEOF DIGUNAKAN UNTUK MENGETAHUI TIPE VARIABEL
