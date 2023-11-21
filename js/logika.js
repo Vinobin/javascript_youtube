a950788fb9e24ac3f3ade10c1f5814639081334c
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2023-11-21 07:53:05 +0700

    V 1.4
    JAVASCRIPT : OPERATOR LOGIKA

---
+++ js/logika.js
@@ -0,0 +1,14 @@
+let a=true;
+let b=false;
+let c=true;
+let d=false;
+console.log(d || b || a);
+console.log(a && b && c); 
+// tanda & adalah false dan akan bernili false jika salah satu bernilai false
+// tanda | adalah true dan akan bernilai true jika salah satu bernilai true
+let x=4;
+let y=4;
+console.log(x==y);
+let lima="6";
+let enam=5;
+console.log(lima==enam);
